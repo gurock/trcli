ef1ae908e0a1578c5f0aa27921a51ed4195770fc
lchoj <lchoj@softserveinc.com>
2021-12-03 12:37:32 +0100

    TRCLI-44 Create FileParser basic class.

--- trcli/readers/junit_xml.py
+++ trcli/readers/junit_xml.py
@@ -1,6 +1,6 @@
-from pathlib import Path
 from junitparser import TestCase, TestSuite, JUnitXml, Attr, JUnitXmlError
 from xml.etree import ElementTree as etree
+from trcli.readers.file_parser import FileParser
 from trcli.data_classes.dataclass_testrail import (
     PropertiesDataclass,
     SuitesDataclass,
@@ -13,34 +13,43 @@ TestSuite.id = Attr("id")
 JUnitXml.id = Attr("id")
 
 
-def add_root_element_to_tree(filename: Path) -> etree:
-    tree = etree.parse(filename)
-    root_elem = tree.getroot()
-    if root_elem.tag == "testsuites":
-        return tree
-    elif root_elem.tag == "testsuite":
-        new_root = etree.Element("testsuites")
-        new_root.insert(0, root_elem)
-        return etree.ElementTree(new_root)
-    else:
-        raise JUnitXmlError("Invalid format.")
+class JunitParser(FileParser):
+    @classmethod
+    def _add_root_element_to_tree(cls, filepath) -> etree:
+        """
+        Because some of junits have XML root as testsuites and some not.
+        This way make sure that we always have testsuites root.
+        """
+        tree = etree.parse(filepath)
+        root_elem = tree.getroot()
+        if root_elem.tag == "testsuites":
+            return tree
+        elif root_elem.tag == "testsuite":
+            new_root = etree.Element("testsuites")
+            new_root.insert(0, root_elem)
+            return etree.ElementTree(new_root)
+        else:
+            raise JUnitXmlError("Invalid format.")
 
-
-def parse_junit(filename: Path) -> SuitesDataclass:
-    xml = JUnitXml.fromfile(filename, parse_func=add_root_element_to_tree)
-    test_suites = []
-    for suite in xml:
-        test_cases = []
-        properties = []
-        for property in suite.properties():
-            properties.append(PropertiesDataclass(property.name, property.value))
-        for case in suite:
-            test_cases.append(
-                TestCaseDataclass(case.name, case.id, case.time, case.result)
-            )
-        test_suites.append(
-            TestSuiteDataclass(suite.name, suite.id, suite.time, test_cases, properties)
+    def parse_file(self) -> SuitesDataclass:
+        xml = JUnitXml.fromfile(
+            self.filepath, parse_func=self._add_root_element_to_tree
         )
+        test_suites = []
+        for suite in xml:
+            test_cases = []
+            properties = []
+            for property in suite.properties():
+                properties.append(PropertiesDataclass(property.name, property.value))
+            for case in suite:
+                test_cases.append(
+                    TestCaseDataclass(case.name, case.id, case.time, case.result)
+                )
+            test_suites.append(
+                TestSuiteDataclass(
+                    suite.name, suite.id, suite.time, test_cases, properties
+                )
+            )
 
-    xml_dataclass = SuitesDataclass(xml.name, xml.id, xml.time, test_suites)
-    return xml_dataclass
+        xml_dataclass = SuitesDataclass(xml.name, xml.id, xml.time, test_suites)
+        return xml_dataclass
