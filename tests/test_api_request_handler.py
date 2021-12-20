2b9dba940444c47905f551490cecd2524d781b5d
lchoj <lchoj@softserveinc.com>
2021-12-20 12:25:33 +0100

    Add more API methods. Fill tests. Refactoring, adding comments and tests

--- tests/test_api_request_handler.py
+++ tests/test_api_request_handler.py
@@ -1,18 +1,25 @@
+import json
+from pathlib import Path
+
 import pytest
 from click.testing import CliRunner
 from requests_mock import Mocker
+from serde.json import from_json
 from tests.helpers.api_client_helpers import TEST_RAIL_URL, create_url
-from tests.test_data.data_provider_post import test_input
 from trcli.api.api_request_handler import ApiRequestHandler
-from trcli.api_client import APIClient
+from trcli.api.api_client import APIClient
 from trcli.cli import Environment
+from trcli.data_classes.dataclass_testrail import TestRailSuite
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def api_request_handler():
     api_client = APIClient(host_name=TEST_RAIL_URL)
     env = Environment()
     env.project = "Test Project"
+    file_json = open(Path(__file__).parent / "test_data/json/api_request_handler.json")
+    json_string = json.dumps(json.load(file_json))
+    test_input = from_json(TestRailSuite, json_string)
     api_request = ApiRequestHandler(env, api_client, test_input)
     yield api_request
 
@@ -26,23 +33,32 @@ class TestAPIHandler:
             "projects": [
                 {"id": 1, "name": "DataHub"},
                 {"id": 2, "name": "Test Project"},
+                {"id": 3, "name": "DataHub"},
             ]
         }
         requests_mock.get(create_url("get_projects"), json=mocked_response)
-        assert api_request_handler.get_project_id("Test Project") == 2
-        assert api_request_handler.get_project_id("Some project") == -1
+        assert api_request_handler.get_project_id("Test Project") == (2, "")
+        assert api_request_handler.get_project_id("DataHub") == (
+            -1,
+            "Given project name matches more than one result.",
+        )
+        assert api_request_handler.get_project_id("Some project") == (
+            -2,
+            "Some project project doesn't exists.",
+        )
 
     def test_check_suite_exists(
         self, api_request_handler: ApiRequestHandler, requests_mock
     ):
         project_id = 3
         mocked_response = [
-            {"id": 4, "name": "Test 1", "description": "Test1", "project_id": 3},
-            {"id": 5, "name": "Test 1", "description": "Test1", "project_id": 3},
+            {"id": 4, "name": "Suite1", "description": "Test1", "project_id": 3},
+            {"id": 5, "name": "Suite2", "description": "Test2", "project_id": 3},
         ]
         requests_mock.get(create_url(f"get_suites/{project_id}"), json=mocked_response)
-        assert api_request_handler.check_suite_id(project_id)
-        assert not api_request_handler.check_suite_id(project_id)
+        assert api_request_handler.check_suite_id(project_id) == (True, "")
+        api_request_handler.suites_data_from_provider.suite_id = 6
+        assert api_request_handler.check_suite_id(project_id) == (False, "")
 
     def test_add_suite(self, api_request_handler: ApiRequestHandler, requests_mock):
         project_id = 3
@@ -53,11 +69,20 @@ class TestAPIHandler:
             "project_id": 1,
             "url": "http:///testrail/index.php?/suites/view/1",
         }
-
+        api_request_handler.suites_data_from_provider.suite_id = None
+        api_request_handler.suites_data_from_provider.name = None
         requests_mock.post(create_url(f"add_suite/{project_id}"), json=mocked_response)
-        for i in api_request_handler.add_suite(project_id):
-            assert i["name"] == mocked_response["name"]
-            assert i["suite_id"] == mocked_response["id"]
+        resources_added, error = api_request_handler.add_suite(project_id)
+
+        assert resources_added[0]["name"] == mocked_response["name"]
+        assert resources_added[0]["suite_id"] == mocked_response["id"]
+        assert error == ""
+
+        assert (
+            api_request_handler.suites_data_from_provider.suite_id
+            == mocked_response["id"]
+        )
+        # assert api_request_handler.suites_data_from_provider.name == mocked_response["name"] #TODO updater in DataProvider doesnt handle "name" property
 
     def test_check_missing_sections(
         self, api_request_handler: ApiRequestHandler, requests_mock
@@ -65,50 +90,122 @@ class TestAPIHandler:
         project_id = 3
         mocked_response = [
             {
-                "id": 2,
-                "suite_id": 5,
-                "name": "This is a new section",
-            },
-            {
-                "id": 4,
-                "suite_id": 5,
-                "name": "This is a new section",
+                "id": 0,
+                "suite_id": 4,
+                "name": "Skipped test",
             },
         ]
 
         requests_mock.get(
-            create_url(f"get_sections/{project_id}&suite_id=1"), json=mocked_response
+            create_url(f"get_sections/{project_id}&suite_id=4"), json=mocked_response
         )
-        assert len(api_request_handler.check_missing_section_id(project_id)) == 0
-        mocked_response[0]["id"] = 66
+        assert (
+            len(api_request_handler.check_missing_section_id(project_id)[0]) == 2
+        ), "There should be one missing section"
+
+        mocked_response[0]["id"] = 1234
         requests_mock.get(
-            create_url(f"get_sections/{project_id}&suite_id=1"), json=mocked_response
+            create_url(f"get_sections/{project_id}&suite_id=4"), json=mocked_response
         )
-        assert api_request_handler.check_missing_section_id(project_id) == [2]
+
+        assert (
+            len(api_request_handler.check_missing_section_id(project_id)[0]) == 1
+        ), "There should be no missing sections"
 
     def test_add_sections(self, api_request_handler: ApiRequestHandler, requests_mock):
         project_id = 3
         mocked_response = {
-            "id": 5,
-            "suite_id": 5,
-            "name": "This is a new section",
+            "id": 1235,
+            "suite_id": 4,
+            "name": "Passed test",
         }
 
         requests_mock.post(
             create_url(f"add_section/{project_id}"), json=mocked_response
         )
-        assert (
-            api_request_handler.add_section(project_id)[0]["section_id"]
-            == mocked_response["id"]
-        )
+        resources_added, error = api_request_handler.add_section(project_id)
 
-    def test_add_case(self, api_request_handler: ApiRequestHandler, requests_mock):
+        assert resources_added[0]["name"] == mocked_response["name"]
+        assert resources_added[0]["section_id"] == mocked_response["id"]
+        assert error == ""
+
+    def test_add_section_and_case(
+        self, api_request_handler: ApiRequestHandler, requests_mock
+    ):
         project_id = 3
-        mocked_response = {
+        mocked_response_for_section = {
+            "id": 12345,
+            "suite_id": 4,
+            "name": "Passed test",
+        }
+        mocked_response_for_case = {
             "id": 3,
-            "suite_id": 5,
-            "name": "This is a new testcase",
+            "suite_id": 4,
+            "section_id": 12345,
+            "title": "testCase2",
         }
 
-        requests_mock.post(create_url(f"add_case/{project_id}"), json=mocked_response)
-        assert api_request_handler.add_case()
+        requests_mock.post(
+            create_url(f"add_section/{project_id}"), json=mocked_response_for_section
+        )
+        requests_mock.post(
+            create_url(f"add_case/{mocked_response_for_case['section_id']}"),
+            json=mocked_response_for_case,
+        )
+        api_request_handler.add_section(project_id)
+        resources_added, error = api_request_handler.add_case()
+
+        assert resources_added[0]["title"] == mocked_response_for_case["title"]
+        assert resources_added[0]["case_id"] == mocked_response_for_case["id"]
+        assert error == ""
+
+    def test_add_run(self, api_request_handler: ApiRequestHandler, requests_mock):
+        project_id = 3
+
+        mocked_response = {
+            "description": None,
+            "id": 81,
+            "include_all": False,
+            "is_completed": False,
+        }
+
+        requests_mock.post(create_url(f"add_run/{project_id}"), json=mocked_response)
+        resources_added, error = api_request_handler.add_run(project_id)
+        assert True  # TODO Fix tests after fixing add_run
+
+    def test_add_results(self, api_request_handler: ApiRequestHandler, requests_mock):
+        run_id = 2  # TODO run_id should be from env or from add run?
+
+        mocked_response = {  # TODO change that or move to file?
+            "offset": 0,
+            "limit": 250,
+            "size": 250,
+            "_links": {
+                "next": "/api/v2/get_results/131071&limit=250&offset=250",
+                "prev": None,
+            },
+            "results": [
+                {
+                    "assignedto_id": 1,
+                    "comment": "This test failed: ..",
+                    "created_by": 1,
+                    "created_on": 1393851801,
+                    "defects": "TR-1",
+                    "elapsed": "5m",
+                    "id": 1,
+                    "status_id": 5,
+                    "test_id": 1,
+                    "version": "1.0RC1",
+                }
+            ],
+        }
+
+        requests_mock.post(
+            create_url(f"add_results_for_cases/{run_id}"), json=mocked_response
+        )
+        resources_added, error = api_request_handler.add_results(run_id)
+        # TODO ASSERT?
+        # TODO Fix tests after fixing add_results
+
+
+# TODO add tests for close_run and check missing test cases ids
--- trcli/api/api_request_handler.py
+++ trcli/api/api_request_handler.py
@@ -1,7 +1,10 @@
-from trcli.api_client import APIClient, APIClientResult
+import itertools
+
+from trcli.api.api_client import APIClient, APIClientResult
 from trcli.cli import Environment
-from trcli.data_classes.dataclass_testrail import SuitesDataclass
+from trcli.data_classes.dataclass_testrail import TestRailSuite
 from trcli.data_providers.api_data_provider import ApiPostProvider
+from typing import List
 
 
 class ApiRequestHandler:
@@ -9,63 +12,121 @@ class ApiRequestHandler:
         self,
         env: Environment,
         api_client: APIClient,
-        suites_data: SuitesDataclass,
+        suites_data: TestRailSuite,
     ):
         self.env = env
         self.client = api_client
-        self.suites_data = suites_data
-        self.suites_data_provider = ApiPostProvider(env, suites_data)
+        self.data_provider = ApiPostProvider(env, suites_data)
+        self.suites_data_from_provider = self.data_provider.suites_input
 
-    def get_project_id(self, project_name):
+    def get_project_id(self, project_name: str) -> (int, str):
+        """
+        Send get_projects with project name
+        :project_name: Project name
+        :returns: Tuple with project id and error string. Project id will be set to -1 if fail.
+        """
 
         response = self.client.send_get("get_projects")
-        available_projects = [
-            project["id"]
-            for project in response.response_text["projects"]
-            if project["name"] == project_name
-        ]
-        if len(available_projects) == 1:
-            return available_projects[0]
+        if not response.error_message:
+            available_projects = [
+                project["id"]
+                for project in response.response_text["projects"]
+                if project["name"] == project_name
+            ]
+            if len(available_projects) == 1:
+                return available_projects[0], response.error_message
+            elif len(available_projects) > 1:
+                return -1, "Given project name matches more than one result."
+            else:
+                return -2, f"{project_name} project doesn't exists."
         else:
-            return -1
+            return -3, response.error_message
 
-    def check_suite_id(self, project_id):
-        suite_id = self.suites_data.id
+    def check_suite_id(self, project_id: int) -> (bool, ""):
+        """
+        Check if suite from DataProvider exist using get_suites endpoint
+        :project_id: project id
+        :returns: True if exists in suites. False if not.
+        """
+        suite_id = self.suites_data_from_provider.suite_id
         response = self.client.send_get(f"get_suites/{project_id}")
-        available_suites = [suite["id"] for suite in response.response_text]
-        return True if suite_id in available_suites else False
+        if not response.error_message:
+            available_suites = [suite["id"] for suite in response.response_text]
+            return (True, "") if suite_id in available_suites else (False, "")
+        else:
+            return None, response.error_message
 
-    def add_suite(self, project_id):
-        add_suite_data = self.suites_data_provider.suite
+    def add_suite(self, project_id: int) -> (List[dict], str):
+        """
+        Adds suites that doesn't have ID's in DataProvider.
+        Runs update_data in data_provider for successfully created resources.
+        :project_id: project_id
+        :returns: Tuple with list of dict created resources and error string.
+        """
+        add_suite_data = self.data_provider.add_suites_data()
         responses = []
+        error = ""
         for body in add_suite_data["bodies"]:
-            responses.append(self.client.send_post(f"add_suite/{project_id}", body))
-        return [
+            response = self.client.send_post(f"add_suite/{project_id}", body)
+            if not response.error_message:
+                responses.append(response)
+            else:
+                error = response.error_message
+                break
+
+        returned_resources = [
             {
                 "suite_id": response.response_text["id"],
                 "name": response.response_text["name"],
             }
             for response in responses
         ]
+        self.data_provider.update_data(suite_data=returned_resources)
+        return returned_resources, error
 
-    def check_missing_section_id(self, project_id):
-        suite_id = self.suites_data.id
-        sections = [section.suite_id for section in self.suites_data.testsuites]
+    def check_missing_section_id(self, project_id: int) -> (List[dict], str):
+        """
+        Check what section id's are missing in DataProvider.
+        :project_id: project_id
+        :returns: Tuple with list of dict created resources and error string.
+        """
+        suite_id = self.suites_data_from_provider.suite_id
+        sections = [
+            section.section_id
+            for section in self.suites_data_from_provider.testsections
+        ]
         response = self.client.send_get(
             f"get_sections/{project_id}&suite_id={suite_id}"
         )
+        if not response.error_message:
+            return (
+                list(
+                    set(sections)
+                    - set([section.get("id") for section in response.response_text])
+                ),
+                response.error_message,
+            )
+        else:
+            return [], response.error_message
 
-        return list(
-            set(sections) - set([section["id"] for section in response.response_text])
-        )
-
-    def add_section(self, project_id):
-        add_sections_data = self.suites_data_provider.sections
+    def add_section(self, project_id: int) -> (List[dict], str):
+        """
+        Add sections that doesn't have ID in DataProvider.
+        Runs update_data in data_provider for successfully created resources.
+        :project_id: project_id
+        :returns: Tuple with list of dict created resources and error string.
+        """
+        add_sections_data = self.data_provider.add_sections_data()
         responses = []
+        error = ""
         for body in add_sections_data["bodies"]:
-            responses.append(self.client.send_post(f"add_section/{project_id}", body))
-
-        return [
+            response = self.client.send_post(f"add_section/{project_id}", body)
+            if not response.error_message:
+                responses.append(response)
+            else:
+                error = response.error_message
+                break
+        returned_resources = [
             {
                 "section_id": response.response_text["id"],
                 "suite_id": response.response_text["suite_id"],
@@ -73,16 +134,28 @@ class ApiRequestHandler:
             }
             for response in responses
         ]
+        self.data_provider.update_data(section_data=returned_resources)
+        return returned_resources, error
 
-    def add_case(self):
-        add_case_data = self.suites_data_provider.cases
+    def add_case(self) -> (List[dict], str):
+        """
+        Add cases that doesn't have ID in DataProvider.
+        Runs update_data in data_provider for successfully created resources.
+        :project_id: project_id
+        :returns: Tuple with list of dict created resources and error string.
+        """
+        add_case_data = self.data_provider.add_cases()
         responses = []
+        error = ""
         for body in add_case_data["bodies"]:
-            responses.append(
-                self.client.send_post(f"add_case/{body.pop('section_id')}", body)
-            )
+            response = self.client.send_post(f"add_case/{body.pop('section_id')}", body)
+            if not response.error_message:
+                responses.append(response)
+            else:
+                error = response.error_message
+                break
 
-        return [
+        returned_resources = [
             {
                 "case_id": response.response_text["id"],
                 "section_id": response.response_text["section_id"],
@@ -90,3 +163,41 @@ class ApiRequestHandler:
             }
             for response in responses
         ]
+        self.data_provider.update_data(case_data=returned_resources)
+        return returned_resources, error
+
+    def add_run(self, project_id: int) -> (List[dict], str):
+        suite_id = self.suites_data_from_provider.suite_id
+        # TODO proper body should be already returned by add_run in DataProvider - To fix in DataProvider
+        # TODO DataProvider will set case_id to -1 if not found in test suite. In full flow this shouldn't happen but dataclass or dataprovider should handle this
+        add_run_data = self.data_provider.add_run()
+        body = {
+            "suite_id": suite_id,
+            "description": " ".join(
+                [single_run["description"] for single_run in add_run_data["bodies"]]
+            ),
+            "case_ids": list(
+                itertools.chain(
+                    *[single_run["case_ids"] for single_run in add_run_data["bodies"]]
+                )
+            ),
+        }
+        response = self.client.send_post(f"add_run/{project_id}", body)
+        return response.response_text.get("id"), response.error_message
+
+    def add_results(self, run_id: int) -> (List[dict], str):
+        # TODO proper body should be already returned by add_run in DataProvider - To fix in DataProvider
+        # TODO DataProvider will set case_id to NONE if not found in test suite. In full flow this shouldn't happen but dataclass or dataprovider should handle this
+        add_results_data = self.data_provider.add_results_for_cases()
+        body = {"results": add_results_data["bodies"]["results"]}
+
+        response = self.client.send_post(f"add_results_for_cases/{run_id}", body)
+        return response.response_text, response.error_message
+
+    def close_run(self, run_id: int):
+        body = {"run_id": run_id}  # TODO handle by dataprovider?
+        response = self.client.send_post(f"close_run/{run_id}", body)
+        return response.response_text, response.error_message
+
+
+# TODO add check mising test cases ids
