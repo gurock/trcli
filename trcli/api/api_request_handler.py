8c1089d3d5e28e9e5621c8ddfc396ca1237aa0c5
d-rede <diogo.v.rede@gmail.com>
2023-02-10 14:58:06 +0000

    Fix automation_id check and add headers to get requests

--- trcli/api/api_request_handler.py
+++ trcli/api/api_request_handler.py
@@ -58,6 +58,8 @@ class ApiRequestHandler:
             if automation_id_field:
                 if automation_id_field["is_active"] is False:
                     return FAULT_MAPPING["automation_id_unavailable"]
+                if not automation_id_field["configs"]:
+                    return None
                 for config in automation_id_field["configs"]:
                     context = config["context"]
                     if context["is_global"] or project_id in context["project_ids"]:
