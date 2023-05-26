d566289c0bd1e210fb2de20a39a84059900ef57b
Book <95514331+Booklynn@users.noreply.github.com>
2023-05-26 08:39:47 +0700

    fixed wrong MatchersParser name

--- trcli/readers/junit_xml.py
+++ trcli/readers/junit_xml.py
@@ -95,7 +95,7 @@ class JunitParser(FileParser):
                     automation_id = f"{case.classname}.{case_name}"
                     if self.case_matcher == MatchersParser.NAME:
                         case_id, case_name = MatchersParser.parse_name_with_id(case_name)
-                    elif self.case_matcher == MatchersParser.NAME_SKIP_MISSING:
+                    elif self.case_matcher == MatchersParser.NAME_SKIP_UNKNOWN:
                         _, case_name = MatchersParser.parse_name_with_id(case_name)
                     for case_props in case.iterchildren(Properties):
                         for prop in case_props.iterchildren(Property):
