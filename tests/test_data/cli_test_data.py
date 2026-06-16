1ab7c31c455c84cc5d2099b4e75d74478dfb7bd3
acuanico-tr-galt <arnel.cuanico@sembi.com>
2026-06-16 17:33:01 +0800

    TRCLI-282: Updated README and CHANGELOG for statuses command, also updated unit tests

--- tests/test_data/cli_test_data.py
+++ tests/test_data/cli_test_data.py
@@ -79,6 +79,7 @@ trcli_description = (
     "    - plans: Query test plans\n"
     "    - runs: Query test runs\n"
     "    - milestones: Query milestones\n"
+    "    - statuses: Query test statuses\n"
     "    - results: Query and update test results\n"
 )
 
